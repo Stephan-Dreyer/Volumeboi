b21330c9dfb58d14c1f754fd9f3ae65cf180e0e4
Stephan <stephan.dreyer1@gmail.com>
2021-07-05 18:41:27 +1000

    optimised code

--- volumeboi/Program.cs
+++ volumeboi/Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.ComponentModel;
 using System.IO.Ports;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 // ReSharper disable SuspiciousTypeConversion.Global
 // ReSharper disable InconsistentNaming
 
@@ -779,134 +781,168 @@ namespace AudioController
         static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int processId);
 
 
-        static SerialPort find_arduino() { 
+        static Tuple<SerialPort, string[]> find_arduino(string[] prev_ports)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            bool found = false;
 
+  
 
-        string[] ports = SerialPort.GetPortNames();
-
-
-        List<SerialPort> Portslist = new List<SerialPort>();
-            List<int> busyports = new List<int>();
-
-
-            for (int i = 0; i<ports.Length; i++)
-
+            if (!ports.SequenceEqual(prev_ports) )
             {
-                Console.WriteLine(ports[i]);
-                Portslist.Add(new SerialPort());
-                Portslist[i].PortName = ports[i];//Set your board COM
-                Portslist[i].BaudRate = 112500;
-                Portslist[i].ReadTimeout = 500;
-                try { Portslist[i].Open();
-    }
-                catch
+                for (int j = 0; j < 10; j++)
                 {
-                    Console.WriteLine("port " + ports[i] + " busy");
-                    busyports.Add(i);
+                    Console.WriteLine("itteration " + j);
+                    List<SerialPort> Portslist = new List<SerialPort>();
+                    List<int> busyports = new List<int>();
+
+                    for (int i = 0; i < ports.Length; i++)
+
+                    {
+                        Console.WriteLine(ports[i]);
+                        Portslist.Add(new SerialPort());
+                        Portslist[i].PortName = ports[i];
+                        Portslist[i].BaudRate = 112500;
+                        Portslist[i].ReadTimeout = 500;
+                        try
+                        {
+                            Portslist[i].Open();
+                        }
+                        catch
+                        {
+                            Console.WriteLine("port " + ports[i] + " busy");
+                            busyports.Add(i);
+                        }
+
+
+
+                    }
+
+                    foreach (int busy in busyports)
+                    {
+                        Portslist.RemoveAt(busy);
+                    }
+
+
+                    for (int i = 0; i < Portslist.Count; i++)
+
+                    {
+                        string ping;
+                        Portslist[i].Write("!");
+                        try
+                        {
+                            ping = Portslist[i].ReadLine();
+                            Console.WriteLine("response recieved from device: " + ping);
+                        }
+                        catch (TimeoutException)
+                        {
+                            Console.WriteLine("Arduino is not at " + Portslist[i].PortName);
+                            continue;
+                        }
+
+
+                        if (ping == "!\r")
+                        {
+                            _serialPort = Portslist[i];
+                            _serialPort.ReadTimeout = -1;
+                            found = true;
+                            
+
+                            Console.WriteLine("Arduino is at " + Portslist[i].PortName);
+                            Portslist.RemoveAt(i);
+                        }
+                    }
+                    // close remaining ports
+                    for (int i = 0; i < Portslist.Count; i++)
+                    {
+                        Console.WriteLine(Portslist[i].PortName + " Closed");
+                        Portslist[i].Close();
+                    }
+                    if (found) break;
+                    Thread.Sleep(500);
                 }
 
-
-
             }
-
-            foreach (int busy in busyports)
-            {
-                Portslist.RemoveAt(busy);
-            }
-
-
-            for (int i = 0; i < Portslist.Count; i++)
-
-{
-    string ping;
-    Portslist[i].Write("!");
-    try
-    {
-        ping = Portslist[i].ReadLine();
-        Console.WriteLine("response recieved from device: " + ping);
-    }
-    catch (TimeoutException)
-    {
-        Console.WriteLine("Arduino is not at " + Portslist[i].PortName);
-        continue;
-    }
-
-
-    if (ping == "!\r")
-    {
-        _serialPort = Portslist[i];
-        _serialPort.ReadTimeout = -1;
-
-
-        Console.WriteLine("Arduino is at " + Portslist[i].PortName);
-        Portslist.RemoveAt(i);
-    }
-}
-// close remaining ports
-for (int i = 0; i < Portslist.Count; i++)
-{
-    Console.WriteLine(Portslist[i].PortName + " Closed");
-    Portslist[i].Close();
-}
-return _serialPort;
+             var result=Tuple.Create(_serialPort, ports);
+            return result;
         }
 
 
         static void Main(string[] args)
 
         {
-            SerialPort _serialPort = find_arduino();
+            int[] prev_volumes = new int[4];
+            string[] initial_ports = { "0" };
+            var output = find_arduino(initial_ports);
+            SerialPort _serialPort = output.Item1;
+            string[] prev_ports =output.Item2;
            
-                while (true)
+
+            while (true)
             {
                 try
                 {
                     bool use_active = true;
-
-                    string a = _serialPort.ReadLine();
-                    Console.WriteLine(a);
-                    List<int> volumes = new List<int>(Array.ConvertAll(a.Split(' '), int.Parse));
+                    string input = _serialPort.ReadLine();
+                    Console.WriteLine(input);
+                    List<int> volumes = new List<int>(Array.ConvertAll(input.Split(' '), int.Parse));
 
 
                     // get active window
                     IntPtr hWnd = GetForegroundWindow();
                     int active_ID;
                     GetWindowThreadProcessId(hWnd, out active_ID);
-                    var discord_processes = Process.GetProcessesByName("Discord");
-                    for (int i = 0; i < discord_processes.Length; i++)
-                    {
-                        if (active_ID == discord_processes[i].Id) use_active = false;// check if discord is active program
-                        AudioManager.SetApplicationVolume(discord_processes[4].Id, volumes[1]);
 
-                    }
-                    var firefox_processes = Process.GetProcessesByName("Firefox");
-                    for (int i = 0; i < firefox_processes.Length; i++)
+                    if (prev_volumes[1] != volumes[1])
                     {
-                        if (active_ID == firefox_processes[i].Id) use_active = false; // check if firefox is active program
-                        AudioManager.SetApplicationVolume(firefox_processes[i].Id, volumes[2]);
+                        var discord_processes = Process.GetProcessesByName("Discord");
+                        for (int i = 0; i < discord_processes.Length; i++)
+                        {
+                            //if (active_ID == discord_processes[i].Id) use_active = false;// check if discord is active program
+                            AudioManager.SetApplicationVolume(discord_processes[4].Id, volumes[1]);
+
+                        }
+                    }
+                    if (prev_volumes[2] != volumes[2])
+                    {
+                        var firefox_processes = Process.GetProcessesByName("Firefox");
+                        for (int i = 0; i < firefox_processes.Length; i++)
+                        {
+                            //if (active_ID == firefox_processes[i].Id) use_active = false; // check if firefox is active program
+                            AudioManager.SetApplicationVolume(firefox_processes[i].Id, volumes[2]);
+                        }
                     }
                     // if firefox or discord arent the acitve program
-                    if (use_active)
+                 
+                        if (prev_volumes[0] != volumes[0])
+                        {
+                            AudioManager.SetApplicationVolume(active_ID, volumes[0]);
+                        }
+                   
+
+                    for (int i = 0; i < prev_volumes.Length; i++)
                     {
-                        AudioManager.SetApplicationVolume(active_ID, volumes[0]);
+                        prev_volumes[i] = volumes[i];
                     }
                 }
                 catch (System.NullReferenceException)
                 {
                     Console.WriteLine("null exception");
-                    _serialPort=find_arduino();
+                    output = find_arduino(prev_ports);
+                     _serialPort = output.Item1;
+                    prev_ports = output.Item2;
+                    Thread.Sleep(5000);
 
                 }
-                catch 
+                catch
                 {
                     Console.WriteLine("was yoiked");
-                    _serialPort = find_arduino();
+                    output = find_arduino(prev_ports);
+                    _serialPort = output.Item1;
+                    prev_ports = output.Item2;
+                    Thread.Sleep(5000);
 
                 }
-
-
-
-
             }
         }
     }
