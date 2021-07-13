14ef33f5cdea0870a8dfa9b80d88b28950da2761
Stephan <stephan.dreyer1@gmail.com>
2021-07-13 17:20:15 +1000

    improved filtering on arduino code

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -3,9 +3,9 @@
 #include <string.h>
 // constants
 #define thresh 10
-#define alpha 0.8
+#define alpha 0.2
 //funciton defs
-int calc_percentage(int pot,int prev);
+int calc_percentage(int pot,int* prev);
 int scale(int pot);
 
 // globlas
@@ -36,41 +36,45 @@ now= millis();
 }
 
 void loop() {
+
  int pot1=analogRead(A4);
  int pot2=analogRead(A5);
  int pot3=analogRead(A6);
  int pot4=analogRead(A7);
 
- pot1_percentage=calc_percentage(pot1,prev_pot1);
- pot2_percentage=calc_percentage(pot2,prev_pot2); 
-  pot3_percentage=calc_percentage(pot3,prev_pot3);
- pot4_percentage=calc_percentage(pot4,prev_pot4); 
 
- int pot1_percentage_debounce=(alpha*pot1_percentage)+((1-alpha)*prev_pot1_percentage);
- int pot2_percentage_debounce=(alpha*pot2_percentage)+((1-alpha)*prev_pot2_percentage);
-  int pot3_percentage_debounce=(alpha*pot3_percentage)+((1-alpha)*prev_pot3_percentage);
- int pot4_percentage_debounce=(alpha*pot4_percentage)+((1-alpha)*prev_pot4_percentage);
+ int pot1_debounce=(alpha*pot1)+((1-alpha)*prev_pot1);
+ int pot2_debounce=(alpha*pot2)+((1-alpha)*prev_pot2);
+ int pot3_debounce=(alpha*pot3)+((1-alpha)*prev_pot3);
+ int pot4_debounce=(alpha*pot4)+((1-alpha)*prev_pot4);
 
- pot1_percentage_debounce=scale(pot1_percentage_debounce);
- pot2_percentage_debounce=scale(pot2_percentage_debounce);
-  pot3_percentage_debounce=scale(pot3_percentage_debounce);
- pot4_percentage_debounce=scale(pot4_percentage_debounce);
+
+
+ pot1_percentage=calc_percentage(pot1_debounce,&prev_pot1);
+ pot2_percentage=calc_percentage(pot2_debounce,&prev_pot2); 
+ pot3_percentage=calc_percentage(pot3_debounce,&prev_pot3);
+ pot4_percentage=calc_percentage(pot4_debounce,&prev_pot4); 
+
+
+ pot1_percentage=scale(pot1_percentage);
+ pot2_percentage=scale(pot2_percentage);
+  pot3_percentage=scale(pot3_percentage);
+ pot4_percentage=scale(pot4_percentage);
 
 
 
  
-itoa(pot1_percentage_debounce,pot1_percentage_str,10);
-itoa(pot2_percentage_debounce,pot2_percentage_str,10);
-itoa(pot3_percentage_debounce,pot3_percentage_str,10);
-itoa(pot4_percentage_debounce,pot4_percentage_str,10);
+itoa(pot1_percentage,pot1_percentage_str,10);
+itoa(pot2_percentage,pot2_percentage_str,10);
+itoa(pot3_percentage,pot3_percentage_str,10);
+itoa(pot4_percentage,pot4_percentage_str,10);
 
 
 
 
-if ((millis()-now)>=180){
-  
+if ((millis()-now)>=130){
  now=millis();
- if ((prev_pot1_percentage!=pot1_percentage_debounce)||(prev_pot2_percentage!=pot2_percentage_debounce)||(prev_pot3_percentage!=pot3_percentage_debounce)||(prev_pot4_percentage!=pot4_percentage_debounce)){
+ if ((prev_pot1_percentage!=pot1_percentage)||(prev_pot2_percentage!=pot2_percentage)||(prev_pot3_percentage!=pot3_percentage)||(prev_pot4_percentage!=pot4_percentage)){
   char volumes[160];
   strcpy(volumes,pot1_percentage_str);
   strcat(volumes," ");
@@ -84,10 +88,10 @@ Serial.println(volumes);
 
 
   }
-   prev_pot1_percentage=pot1_percentage_debounce;
-  prev_pot2_percentage=pot2_percentage_debounce;
-   prev_pot3_percentage=pot3_percentage_debounce;
-  prev_pot4_percentage=pot4_percentage_debounce;
+   prev_pot1_percentage=pot1_percentage;
+  prev_pot2_percentage=pot2_percentage;
+   prev_pot3_percentage=pot3_percentage;
+  prev_pot4_percentage=pot4_percentage;
 }
 
 Serial.flush();
@@ -110,22 +114,14 @@ Serial.flush();
 
 int scale(int pot){
   
- pot=floor((double)pot/0.98);
- pot=min(pot,100);
- pot=max(pot,0);
+pot=floor((double)pot/0.99);
+pot=min(pot,100);
+pot=max(pot,0);
 return pot;
 }
-int calc_percentage(int pot,int prev){
- if ((abs(pot-prev)>=thresh)){
-//  Serial.print("pot value ");
-//    Serial.println(pot);
-//      Serial.print("prev pot value ");
-//    Serial.println(prev);
-//      Serial.print("entry gate ");
-//    Serial.println((abs(pot-prev)));
- prev=pot;
+
+int calc_percentage(int pot,int* prev){
+ *prev=pot;
    int pot_percentage=floor(((double)pot/(double)1023)*100);
    return pot_percentage;
-}
-else return NULL;
 }
